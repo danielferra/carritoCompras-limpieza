e578a73866aa117c9553cce8941e8db9470cf76f
danielferra <dferraioli@hotmail.com>
2022-09-14 10:20:05 -0300

    agregue archivo sweetAlert js

---
+++ js/sweetAlert.js
@@ -0,0 +1,11 @@
+Swal.fire({
+    title: 'Bienvenidos!',
+    text: 'Elije el producto',
+    padding: '1rem',
+    imageUrl: '/images/pexels.jpg',
+    imageWidth: 400,
+    backdrop: 'true',
+    position: 'center',
+    confirmButtonColor: '#0d6efd',
+    imageAlt: 'imagen',
+  })
